47debc35b07eaba4442551f81e984bacb6549949
github eggBoy <github>
2025-10-30 17:40:22 +0900

    <概要> トップダウン方式によるパズルゲーム開発3(完了) <詳細> main関数，go_dungeon関数，do_battle関数の変更．print_monster_name関数の新規追加． ①敵モンスターをディクショナリで作成する． ②敵モンスターをリストで管理する． ③敵モンスターの名前は，~スライム~のように，属性を表す記号が前後に付き，さらに属性を表す色で表示される．

--- study_python/python_basic/puzmon3.py
+++ study_python/python_basic/puzmon3.py
@@ -5,16 +5,42 @@
 #インポート
 
 #グローバル変数の定義
+#属性定義
+ELEMENT_SYMBOLS = {
+    '火' : '$',
+    '水' : '~',
+    '風' : '@',
+    '土' : '#',
+    '命' : '&',
+    '無' : ' ',
+}
+ELEMENT_COLORS = {
+    '火' : '1',
+    '水' : '6',
+    '風' : '2',
+    '土' : '3',
+    '命' : '5',
+    '無' : '7',
+}
 
 #関数宣言
+def print_monster_name(e_mon):
+    monster_name = e_mon['name']
+    monster_element = e_mon['element']
+    symbol = ELEMENT_SYMBOLS[monster_element]
+    color  = ELEMENT_COLORS[monster_element]
+    print(f'\033[3{color}m{symbol}{monster_name}{symbol}\033[0m', end='')       #カラー文字表示：\033[3色コードm出力文字列\033[0m  #カラー背景表示：\033[4色コードm出力文字列\033[0m  ※「0\33[0m」はカラー指示リセット．
+
 def do_battle(e_monster):
-    print(f'{e_monster}が現れた！')
-    print(f'{e_monster}を倒した！')
+    print_monster_name(e_monster)
+    print(f'が現れた！')
+    print_monster_name(e_monster)
+    print(f'を倒した！')
     return 1
 
-
-def go_dungeon(player_name, enemy):
-    e_monsters = ['スライム', 'ゴブリン', 'オオコウモリ', 'ウェアウルフ', 'ドラゴン']
+def go_dungeon(player_name, enemies):
+    #e_monsters = ['スライム', 'ゴブリン', 'オオコウモリ', 'ウェアウルフ', 'ドラゴン']
+    e_monsters = enemies
     win_cnt = 0
     print(f'{player_name}はダンジョンに到着した')
     #倒したモンスター数カウント
@@ -23,7 +49,7 @@ def go_dungeon(player_name, enemy):
         if is_win == True:
             win_cnt += 1
     print(f'{player_name}はダンジョンを制覇した')
-    return 5
+    return win_cnt
 
 def main():
     is_player = False
@@ -37,9 +63,49 @@ def main():
     #ゲームスタート
     print('*** Puzzle & Monsters ***')
     #敵モンスターの作成
-
+    slime = {
+        'name' : 'スライム',
+        'hp' : 100,
+        'max_hp' : 100,
+        'element' : '水',
+        'ap' : 10,
+        'dp' : 1
+    }
+    goblin = {
+        'name' : 'ゴブリン',
+        'hp' : 200,
+        'max_hp' : 200,
+        'element' : '土',
+        'ap' : 20,
+        'dp': 5
+    }
+    giantbat = {
+        'name' : 'オオコウモリ',
+        'hp' : 300,
+        'max_hp' : 300,
+        'element' : '風',
+        'ap' : 30,
+        'dp' : 10
+    }
+    werewolf = {
+        'name' : 'ウェアウルフ',
+        'hp' : 400,
+        'max_hp' :400,
+        'element' : '風',
+        'ap' : 40,
+        'dp' : 15
+    }
+    dragon = {
+        'name' : 'ドラゴン',
+        'hp' : 600,
+        'max_hp' : 600,
+        'element' : '火',
+        'ap' : 50,
+        'dp' : 20
+    }
+    enemies = [slime, goblin, giantbat, werewolf, dragon]
     #倒したモンスター数のカウントおよび表示
-    num_defeated = go_dungeon(player)
+    num_defeated = go_dungeon(player, enemies)
     if num_defeated == 5:
         print('*** GAME CLEARED!! ***')
     elif num_defeated < 5:
