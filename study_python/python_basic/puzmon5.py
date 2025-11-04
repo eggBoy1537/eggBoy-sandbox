b99a3048b3617836b09df4cbf9d2e8ff4b3b2134
github eggBoy <github>
2025-11-04 17:24:16 +0900

    <概要> トップダウン方式によるパズルゲーム開発5：バトルの基本的な流れの開発 <詳細> do_battle関数の変更．on_player_turn関数，on_enemy_turn関数，do_attack関数，do_enemy_attack関数の新規追加． ①バトルは敵と味方が交互に攻撃を繰り返す． ②味方の攻撃は入力した文字列に応じてダメージを決定する．宝石はまだ実装しない． ③敵のターンではパーティは常に固定ダメージを受ける．

---
+++ study_python/python_basic/puzmon5.py
@@ -0,0 +1,234 @@
+'''
+作成日：2025年11月4日
+'''
+
+#インポート
+import random
+
+#グローバル変数の定義
+#属性定義
+ELEMENT_SYMBOLS = {
+    '火' : '🔥',      #$
+    '水' : '💧',      #~
+    '風' : '🌀',      #@
+    '土' : '🏜',      ##
+    '命' : '🧡',      #&
+    '無' : ' ',
+}
+ELEMENT_COLORS = {
+    '火' : '1',
+    '水' : '6',
+    '風' : '2',
+    '土' : '3',
+    '命' : '5',
+    '無' : '7',
+}
+
+#関数宣言
+def on_player_turn(party, e_mon):
+    print(f'【{party['プレイヤー名']}のターン】（HP = {party['HP']}）')
+    cmd = input('コマンド?>')
+    do_attack(e_mon, cmd)
+    return None
+
+def on_enemy_turn(party, e_mon):
+    print(f'【', end = '')
+    print_monster_name(e_mon)
+    print(f'のターン】（HP = {e_mon['hp']}）')
+    do_enemy_attack(party)
+    return None
+
+def do_attack(e_mon, command):
+    base_dmg = hash(command) % 50                                   #入力コマンドをハッシュ関数で変換し基準ダメージを計算．
+    dmg = int(random.uniform(base_dmg * 0.9, base_dmg * 1.1))       #実際に敵に与えるダメージは基準ダメージの±10%．小数点以下切り捨て．
+    e_mon['hp'] -= dmg
+    print(f'{dmg}のダメージを与えた')
+    return None
+
+def do_enemy_attack(party):
+    dmg = 10
+    party['HP'] -= dmg
+    print(f'{dmg}のダメージを受けた')
+    return None
+
+def print_monster_name(e_mon):
+    e_mon_name = e_mon['name']
+    e_mon_element = e_mon['element']
+    symbol = ELEMENT_SYMBOLS[e_mon_element]
+    color  = ELEMENT_COLORS[e_mon_element]
+    print(f'\033[3{color}m{symbol}{e_mon_name}{symbol}\033[0m', end='')       #カラー文字表示：\033[3色コードm出力文字列\033[0m  #カラー背景表示：\033[4色コードm出力文字列\033[0m  ※「0\33[0m」はカラー指示リセット．
+    return None
+
+def do_battle(party, e_mon):
+    win_cnt = 0
+    print_monster_name(e_mon)
+    print(f'が現れた！')
+    while party['HP'] > 0 and e_mon['hp'] > 0:
+        if party['HP'] <= 0:
+            break
+        on_player_turn(party, e_mon)
+        if e_mon['hp'] <= 0:
+            break
+        on_enemy_turn(party, e_mon)
+    if e_mon['hp'] <= 0:
+        print_monster_name(e_mon)
+        print(f'を倒した！')
+        win_cnt = 1
+    elif party['HP'] <= 0:
+        print(f'パーティのHPは0になった')
+        win_cnt = 0
+    return win_cnt
+
+def show_party(party_inf):
+    a_mon_list = party_inf['味方モンスター']
+    print('<パーティ編成>----------------------')
+    for a_mon_inf in a_mon_list:
+        a_mon_name = a_mon_inf['name']
+        a_mon_hp = a_mon_inf['hp']
+        a_mon_ap = a_mon_inf['ap']
+        a_mon_dp = a_mon_inf['dp']
+        a_mon_element = a_mon_inf['element']
+        symbol = ELEMENT_SYMBOLS[a_mon_element]
+        color = ELEMENT_COLORS[a_mon_element]
+        print(f'\033[3{color}m{symbol}{a_mon_name}{symbol}\033[0m HP = {a_mon_hp} 攻撃 = {a_mon_ap} 防御 = {a_mon_dp}')
+    print('----------------------------------')
+    return None
+
+def go_dungeon(party, enemies):
+    win_cnt = 0
+    print(f'{party['プレイヤー名']}のパーティ(HP = {party['HP']})はダンジョンに到着した')
+    show_party(party)
+    #倒したモンスター数カウント
+    for mon in enemies:
+        is_win = do_battle(party, mon)
+        if is_win == True:
+            win_cnt += 1
+        if party['HP'] <= 0:
+            print(f'{party['プレイヤー名']}はダンジョンから逃げ出した')
+            break
+        else:
+            print(f'{party['プレイヤー名']}はさらに奥へと進んだ')
+            print('=======================')
+    if win_cnt == 5:
+        print(f'{party['プレイヤー名']}はダンジョンを制覇した')
+    return win_cnt
+
+def organize_party(player_name, allies):
+    hp_list = list()
+    max_hp_list = list()
+    dp_list = list()
+    for a_mon in allies:
+        hp_list.append(a_mon['hp'])
+        max_hp_list.append(a_mon['max_hp'])
+        dp_list.append(a_mon['dp'])
+
+    hp_all = sum(hp_list)
+    max_hp_all = sum(max_hp_list)
+    dp_all = sum(dp_list) / len(allies)
+    party = {
+        'プレイヤー名' : player_name,
+        '味方モンスター' : allies,
+        'HP' : hp_all,
+        '最大HP' : max_hp_all,
+        '防御力' : dp_all
+    }
+    return party
+
+def main():
+    is_player = False
+    #プレイヤー名入力および入力確認
+    while is_player == False:
+        player = input('プレイヤーを入力してください>')
+        if player == '':
+            print('エラー：プレイヤー名を入力してください．')
+        else:
+            is_player = True
+    #ゲームスタート
+    print('*** Puzzle & Monsters ***')
+    #味方モンスターの作成
+    bluedragon = {
+        'name' : '青龍',
+        'hp' : 150,
+        'max_hp' : 150,
+        'element' : '風',
+        'ap' : 15,
+        'dp' : 10
+    }
+    phoenix = {
+        'name' : '朱雀',
+        'hp' : 150,
+        'max_hp' : 150,
+        'element' : '火',
+        'ap' : 25,
+        'dp' : 10
+    }
+    whitetiger = {
+        'name' : '白虎',
+        'hp' : 150,
+        'max_hp' : 150,
+        'element' : '土',
+        'ap' : 20,
+        'dp' : 5
+    }
+    blacktortoise = {
+        'name' : '玄武',
+        'hp' : 150,
+        'max_hp' : 150,
+        'element' : '水',
+        'ap' : 20,
+        'dp' : 15
+    }
+    allies = [bluedragon, phoenix, whitetiger, blacktortoise]
+    party = organize_party(player, allies)
+    #敵モンスターの作成
+    slime = {
+        'name' : 'スライム',
+        'hp' : 100,
+        'max_hp' : 100,
+        'element' : '水',
+        'ap' : 10,
+        'dp' : 1
+    }
+    goblin = {
+        'name' : 'ゴブリン',
+        'hp' : 200,
+        'max_hp' : 200,
+        'element' : '土',
+        'ap' : 20,
+        'dp': 5
+    }
+    giantbat = {
+        'name' : 'オオコウモリ',
+        'hp' : 300,
+        'max_hp' : 300,
+        'element' : '風',
+        'ap' : 30,
+        'dp' : 10
+    }
+    werewolf = {
+        'name' : 'ウェアウルフ',
+        'hp' : 400,
+        'max_hp' :400,
+        'element' : '風',
+        'ap' : 40,
+        'dp' : 15
+    }
+    dragon = {
+        'name' : 'ドラゴン',
+        'hp' : 600,
+        'max_hp' : 600,
+        'element' : '火',
+        'ap' : 50,
+        'dp' : 20
+    }
+    enemies = [slime, goblin, giantbat, werewolf, dragon]
+    #倒したモンスター数のカウントおよび表示
+    num_defeated = go_dungeon(party, enemies)
+    if num_defeated == 5:
+        print('*** GAME CLEARED!! ***')
+    elif num_defeated < 5:
+        print('*** GAME OVER!! ***')
+    print(f'倒したモンスター数={num_defeated}')
+
+#メイン関数の呼び出し
+main()
