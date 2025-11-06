2e278ef163a151eaab8c793475924c0ab0a7199c
github eggBoy <github>
2025-11-06 19:22:33 +0900

    <概要> 課題7 コマンド入力に基づいた宝石移動の実装(ファイル作成のみ) <詳細> 作業に向けてファイルを作成．

---
+++ study_python/python_basic/puzmon7.py
@@ -0,0 +1,184 @@
+'''
+作成日：2025年11月6日
+'''
+
+#インポート
+import random
+
+#グローバル変数の定義
+#属性定義
+ELEMENT_SYMBOLS = {
+    '火' : '$',      #$🔥
+    '水' : '~',      #~💧
+    '風' : '@',      #@🌀
+    '土' : '#',      ##🏜
+    '命' : '&',      #&🧡
+    '無' : ' ',
+}
+ELEMENT_COLORS = {
+    '火' : '1',
+    '水' : '6',
+    '風' : '2',
+    '土' : '3',
+    '命' : '5',
+    '無' : '7',
+}
+
+#関数宣言
+#ユーティリティ関数
+def print_monster_name(mon):
+    global ELEMENT_SYMBOLS, ELEMENT_COLORS
+    mon_element = mon['element']
+    symbol = ELEMENT_SYMBOLS[mon_element]
+    color  = ELEMENT_COLORS[mon_element]
+    print(f'\033[4{color}m{symbol}{mon['name']}{symbol}\033[0m ', end='')       #カラー背景表示：\033[4色コードm出力文字列\033[0m  #カラー背景表示：\033[4色コードm出力文字列\033[0m  ※「0\33[0m」はカラー指示リセット．
+    return None
+
+def fill_gems(gems):
+    global ELEMENT_SYMBOLS
+    new_gems = gems[:]          #gemsのコピーをnew_gemsとして定義．
+    now_gems_len = len(gems)
+    req_gems = 14 - now_gems_len
+    for i in range(req_gems):
+        random_n = int(random.uniform(0, 4))        #各属性を0~4としてスロット(リスト)を作成
+        new_gems.append(random_n)
+    return new_gems
+
+#メイン関数
+def main():
+    is_player = False
+    #プレイヤー名入力および入力確認
+    while is_player == False:
+        player = input('プレイヤーを入力してください>')
+        if player == '':
+            print('エラー：プレイヤー名を入力してください．')
+        else:
+            is_player = True
+    #ゲームスタート
+    print('*** Puzzle & Monsters ***')
+    #味方モンスターの作成
+    bluedragon = {'name' : '青龍', 'hp' : 150, 'max_hp' : 150, 'element' : '風', 'ap' : 15, 'dp' : 10}
+    phoenix = {'name' : '朱雀', 'hp' : 150, 'max_hp' : 150, 'element' : '火', 'ap' : 25, 'dp' : 10}
+    whitetiger = {'name' : '白虎', 'hp' : 150, 'max_hp' : 150, 'element' : '土', 'ap' : 20, 'dp' : 5}
+    blacktortoise = {'name' : '玄武', 'hp' : 150, 'max_hp' : 150, 'element' : '水', 'ap' : 20, 'dp' : 15}
+    allies = [bluedragon, phoenix, whitetiger, blacktortoise]
+    party = organize_party(player, allies)
+    #敵モンスターの作成
+    slime = {'name' : 'スライム', 'hp' : 100, 'max_hp' : 100, 'element' : '水', 'ap' : 10, 'dp' : 1}
+    goblin = {'name' : 'ゴブリン', 'hp' : 200, 'max_hp' : 200, 'element' : '土', 'ap' : 20, 'dp': 5}
+    giantbat = {'name' : 'オオコウモリ', 'hp' : 300, 'max_hp' : 300, 'element' : '風', 'ap' : 30, 'dp' : 10}
+    werewolf = {'name' : 'ウェアウルフ', 'hp' : 400, 'max_hp' :400, 'element' : '風', 'ap' : 40, 'dp' : 15}
+    dragon = {'name' : 'ドラゴン', 'hp' : 600, 'max_hp' : 600, 'element' : '火', 'ap' : 50, 'dp' : 20}
+    enemies = [slime, goblin, giantbat, werewolf, dragon]
+    #倒したモンスター数のカウントおよび表示
+    num_defeated = go_dungeon(party, enemies)
+    if num_defeated == 5:
+        print('*** GAME CLEARED!! ***')
+    else:
+        print('*** GAME OVER!! ***')
+    print(f'倒したモンスター数={num_defeated}')
+
+#ビジネスロジック関数
+def organize_party(player_name, allies):
+    hp_all = sum([a_mon['hp'] for a_mon in allies])
+    max_hp_all = sum([a_mon['max_hp'] for a_mon in allies])
+    dp_all = sum([a_mon['dp'] for a_mon in allies]) / len(allies)
+    party = {'プレイヤー名' : player_name, '味方モンスター' : allies, 'HP' : hp_all, '最大HP' : max_hp_all, '防御力' : dp_all}
+    return party
+
+def go_dungeon(party, enemies):
+    win_cnt = 0
+    print(f'{party['プレイヤー名']}のパーティ(HP = {party['最大HP']})はダンジョンに到着した')
+    show_party(party)
+    #倒したモンスター数カウント
+    for mon in enemies:
+        win_cnt += do_battle(party, mon)
+        if party['HP'] <= 0:
+            print(f'{party['プレイヤー名']}はダンジョンから逃げ出した')
+            return win_cnt
+        else:
+            print(f'{party['プレイヤー名']}はさらに奥へと進んだ')
+            print('=======================')
+    print(f'{party['プレイヤー名']}はダンジョンを制覇した')
+    return win_cnt
+
+def show_party(party_inf):
+    a_mon_list = party_inf['味方モンスター']
+    print('<パーティ編成>----------------------')
+    for a_mon in a_mon_list:
+        print_monster_name(a_mon)
+        print(f'HP = {a_mon['hp']} 攻撃 = {a_mon['ap']} 防御 = {a_mon['dp']}')
+    print('----------------------------------')
+    return None
+
+def do_battle(party, e_mon):
+    print_monster_name(e_mon)
+    print(f'が現れた！')
+    gems = []
+    gems = fill_gems(gems)
+    battle_field = {'enemy': e_mon, 'party' : party, 'gems' : gems}
+    while True:
+        on_player_turn(battle_field)
+        if e_mon['hp'] <= 0:
+            break
+        on_enemy_turn(party, e_mon)
+        if party['HP'] <= 0:
+            print(f'パーティのHPは0になった')
+            return 0                            #パーティが負けた時は倒した敵の数をカウントアップしないため0を返して即関数から抜ける．
+    print_monster_name(e_mon)
+    print(f'を倒した！')
+    return 1
+
+def on_player_turn(battle_field):
+    (party, e_mon) = (battle_field['party'], battle_field['enemy'])
+    print(f'【{party['プレイヤー名']}のターン】（HP = {party['HP']} / {party['最大HP']}）')
+    show_battle_field(battle_field)
+    cmd = input('コマンド? >> ')
+    do_attack(e_mon, cmd)
+    return None
+
+def show_battle_field(battle_field):
+    (party, e_mon) = (battle_field['party'], battle_field['enemy'])
+    print('バトルフィールド')
+    print_monster_name(e_mon)
+    print(f'HP = {e_mon['hp']} / {e_mon['max_hp']}\n')
+    for a_mon_name in party['味方モンスター']:
+        print_monster_name(a_mon_name)
+    print(f'\nHP = {party['HP']} / {party['最大HP']}')
+    print('---------------------------')
+    print('A B C D E F G H I J K L M N')
+    print_gems(battle_field['gems'])
+    print('\n---------------------------')
+    return None
+
+def print_gems(gems):
+    global ELEMENT_SYMBOLS, ELEMENT_COLORS
+    gems_symbol = [symbol for symbol in ELEMENT_SYMBOLS.values()]
+    gems_color = [color for color in ELEMENT_COLORS.values()]
+    gems_slot = fill_gems(gems)
+    for s in gems_slot:
+        print(f'\033[4{gems_color[s]}m{gems_symbol[s]}\033[0m ', end='')
+    return None
+
+def do_attack(e_mon, command):
+    base_dmg = hash(command) % 50                                   #入力コマンドをハッシュ関数で変換し基準ダメージを計算．
+    dmg = int(random.uniform(base_dmg * 0.9, base_dmg * 1.1))       #実際に敵に与えるダメージは基準ダメージの±10%．小数点以下切り捨て．
+    e_mon['hp'] -= dmg
+    print(f'ダミー攻撃で相手に{dmg}のダメージを与えた\n')
+    return None
+
+def on_enemy_turn(party, e_mon):
+    print(f'【', end = '')
+    print_monster_name(e_mon)
+    print(f'のターン】（HP = {e_mon['hp']} / {e_mon['max_hp']}）')
+    do_enemy_attack(party)
+    return None
+
+def do_enemy_attack(party):
+    dmg = 200
+    party['HP'] -= dmg
+    print(f'{dmg}のダメージを受けた\n')
+    return None
+
+#メイン関数の呼び出し
+main()
